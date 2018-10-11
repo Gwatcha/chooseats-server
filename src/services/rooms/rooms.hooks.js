207834f41cae2ba81a4777c9518a42305c06b843
Timothy Leung <me@timothyleung.ca>
2018-10-11 12:20:10 -0700

    Add restrictions to find/get/update/patch for rooms

--- src/services/rooms/rooms.hooks.js
+++ src/services/rooms/rooms.hooks.js
@@ -1,20 +1,21 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
-const hooks = require('feathers-authentication-hooks');
 
-const includeUser = require('../../hooks/include_user');
+const includeUser = require('./hooks/include_user');
 const createRoomID = require('./hooks/create_room_id');
 const associateRoomAdmin = require('./hooks/associate_room_admin');
 const joinRoom = require('./hooks/join_room');
+const restrictUsers = require('./hooks/restrict_to_room_users');
+const queryWithCurrentUser = require('./hooks/query_with_current_user');
 
 module.exports = {
   before: {
     all: [authenticate('jwt')],
-    find: [includeUser],
-    get: [includeUser],
+    find: [includeUser, queryWithCurrentUser()],
+    get: [includeUser, restrictUsers()],
     create: [createRoomID],
-    update: [],
-    patch: [joinRoom],
-    remove: []
+    update: [restrictUsers()],
+    patch: [joinRoom, restrictUsers()],
+    remove: [restrictUsers()]
   },
 
   after: {
