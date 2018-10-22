47c352aae37ecd046e3249d9fd75ea9b19a517c0
Timothy Leung <timothy_leung96@hotmail.com>
2018-10-21 19:49:18 -0700

    Converts tabs to spaces

--- src/services/rooms/hooks/join_room.js
+++ src/services/rooms/hooks/join_room.js
@@ -11,7 +11,7 @@ module.exports = async context => {
         include: [{ model: UserModel, through: { attributes: ['admin'], as: 'role' }, attributes: ['id', 'email'] }]
       });
       room.addUser(context.params.user.id, { through: { admin: false } });
-			context.result = room;
+      context.result = room;
 
       // Emit an event saying this user is joining this rooms channel. A
       // listener defined in channels.js will add this user to the channel.
