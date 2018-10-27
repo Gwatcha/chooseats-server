ea36ef2ca0b7beb79b8b1aac0b4270b6dce3a1de
Gwatcha <michael.ampm@gmail.com>
2018-10-26 18:04:16 -0700

    fixed some code style suggestions by codacy

--- migrations/models/index.js
+++ migrations/models/index.js
@@ -6,6 +6,6 @@ const models = sequelize.models;
 // The export object must be a dictionary of model names -> models
 // It must also include sequelize (instance) and Sequelize (constructor) properties
 module.exports = Object.assign({
-    Sequelize,
-    sequelize
+  Sequelize,
+  sequelize
 }, models);
