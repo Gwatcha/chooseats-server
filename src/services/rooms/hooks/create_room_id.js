80bcd487b857efe141cbc8221f032329e9c19d49
Timothy Leung <me@timothyleung.ca>
2018-10-31 11:11:46 -0700

    Modifies magic number to be a constant

--- src/services/rooms/hooks/create_room_id.js
+++ src/services/rooms/hooks/create_room_id.js
@@ -1,9 +1,10 @@
 const generate = require('nanoid/generate');
 const alphabet = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const roomCodeLength = 6;
 
 module.exports = context => {
   console.log(context);
-  context.data.roomCode = generate(alphabet, 6);
+  context.data.roomCode = generate(alphabet, roomCodeLength);
   return (context);
 };
 
