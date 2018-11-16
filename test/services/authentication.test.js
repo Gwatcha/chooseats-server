005b4ef1c0c0d4a087ac69a904d632a0cec8d9ad
Timothy Leung <timothy_leung96@hotmail.com>
2018-11-16 14:33:30 -0800

    Adds user tests

---
+++ test/services/authentication.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const app = require('../../src/app');
+
+describe('\'authentication\' service', () => {
+    it('registered the service', () => {
+        const service = app.service('authentication');
+
+        assert.ok(service, 'Registered the service');
+    });
+
+    it('retrieve authentication token', async () => {
+        const user = await app.service('users').create({
+            email: 'testauthentication@example.com',
+            password: 'secretpassword'
+        });
+
+        // broken
+        // const result = await app.authenticate({ email: user.email, password: user.password, strategy: 'local' });
+
+        // assert(result.accessToken);
+
+        await app.service('users').remove(user.id, { user });
+    });
+});
--- test/services/users.test.js
+++ test/services/users.test.js
@@ -2,9 +2,41 @@ const assert = require('assert');
 const app = require('../../src/app');
 
 describe('\'users\' service', () => {
+
   it('registered the service', () => {
     const service = app.service('users');
 
     assert.ok(service, 'Registered the service');
+  });
+
+  it('creates a user, encrypts password', async () => {
+    const email = 'testaccount@example.com';
+    const password = 'password1';
+
+    const user = await app.service('users').create({
+      email: email,
+      password: password
+    });
+
+    assert.ok(user.email === email);
+    assert.ok(user.password !== 'secret');
+
+    const params = { user };
+    await app.service('users').remove(user.id, params);
+  });
+
+  it('removes password for external requests', async () => {
+    // Setting `provider` indicates an external request
+    const params = { provider: 'rest' };
+
+    const user = await app.service('users').create({
+      email: 'testaccount123@example.com',
+      password: 'secretpassword'
+    }, params);
+
+    // Make sure password has been removed
+    assert.ok(!user.password);
+
+    await app.service('users').remove(user.id, { user });
   });
 });
