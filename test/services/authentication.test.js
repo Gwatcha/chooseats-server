9b094d4f82475b9fa40c9c8263e21a190a12401d
Timothy Leung <timothy_leung96@hotmail.com>
2018-11-18 15:45:30 -0800

    Fixes eslint errors

--- test/services/authentication.test.js
+++ test/services/authentication.test.js
@@ -2,23 +2,23 @@ const assert = require('assert');
 const app = require('../../src/app');
 
 describe('\'authentication\' service', () => {
-    it('registered the service', () => {
-        const service = app.service('authentication');
+  it('registered the service', () => {
+    const service = app.service('authentication');
 
-        assert.ok(service, 'Registered the service');
+    assert.ok(service, 'Registered the service');
+  });
+
+  it('retrieve authentication token', async () => {
+    const user = await app.service('users').create({
+      email: 'testauthentication@example.com',
+      password: 'secretpassword'
     });
 
-    it('retrieve authentication token', async () => {
-        const user = await app.service('users').create({
-            email: 'testauthentication@example.com',
-            password: 'secretpassword'
-        });
+    // broken
+    // const result = await app.authenticate({ email: user.email, password: user.password, strategy: 'local' });
 
-        // broken
-        // const result = await app.authenticate({ email: user.email, password: user.password, strategy: 'local' });
+    // assert(result.accessToken);
 
-        // assert(result.accessToken);
-
-        await app.service('users').remove(user.id, { user });
-    });
+    await app.service('users').remove(user.id, { user });
+  });
 });
