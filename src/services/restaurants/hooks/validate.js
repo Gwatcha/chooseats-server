52630e823193aeeb8edce260eadd2a3ba03f5428
gwatcha <michael.ampm@gmail.com>
2018-11-18 16:53:38 -0800

    fixed room assertion thing, not running eslint on tests now as well

--- src/services/restaurants/hooks/validate.js
+++ src/services/restaurants/hooks/validate.js
@@ -8,11 +8,11 @@ module.exports = async context => {
   // Get the rooms for this roomId
   const roomsModel = context.app.service('rooms').Model;
   await roomsModel.findOne({
-    where: { id : context.data.roomId, roomState : 'voting' }
+    where: { id : context.data.roomId }
   }).then( (room) => {
     if ( room == undefined ) {
       console.log('room is undefined!');
-      throw new BadRequest('The room for this restaurant either does not exist, or is not in a voting state.', { roomId : context.data.roomId });
+      throw new BadRequest('The room for this restaurant does not exist.', { roomId : context.data.roomId });
     }
   });
 
